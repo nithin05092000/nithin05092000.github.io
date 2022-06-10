4459bf38e6a3081dd0d818fd2e31f0a9d8d2726f
nithin05092000 <nithinabhi14@gmail.com>
2022-06-10 14:20:29 +0530

    updated

---
+++ application/views/backend/admin/navigation.php
@@ -0,0 +1,159 @@
+<?php
+$status_wise_courses = $this->crud_model->get_status_wise_courses();
+?>
+<!-- ========== Left Sidebar Start ========== -->
+<div class="left-side-menu left-side-menu-detached">
+	<div class="leftbar-user">
+		<a href="javascript: void(0);">
+			<img src="<?php echo $this->user_model->get_user_image_url($this->session->userdata('user_id')); ?>" alt="user-image" height="42" class="rounded-circle shadow-sm">
+			<?php
+			$admin_details = $this->user_model->get_all_user($this->session->userdata('user_id'))->row_array();
+			?>
+			<span class="leftbar-user-name"><?php echo $admin_details['first_name'] . ' ' . $admin_details['last_name']; ?></span>
+		</a>
+	</div>
+
+	<!--- Sidemenu -->
+	<ul class="metismenu side-nav side-nav-light">
+
+		<li class="side-nav-title side-nav-item"><?php echo get_phrase('navigation'); ?></li>
+
+		<li class="side-nav-item <?php if ($page_name == 'dashboard') echo 'active'; ?>">
+			<a href="<?php echo site_url('admin/dashboard'); ?>" class="side-nav-link">
+				<i class="dripicons-view-apps"></i>
+				<span><?php echo get_phrase('dashboard'); ?></span>
+			</a>
+		</li>
+
+		<?php if (has_permission('course')) : ?>
+			<li class="side-nav-item <?php if ($page_name == 'courses' || $page_name == 'course_add' || $page_name == 'course_edit' || $page_name == 'categories' || $page_name == 'category_add' || $page_name == 'category_edit' || $page_name == 'coupons' || $page_name == 'coupon_add' || $page_name == 'coupon_edit' || $page_name == 'add_bundle' || $page_name == 'manage_course_bundle' || $page_name == 'edit_bundle' || $page_name == 'active_bundle_subscription_report' || $page_name == 'expire_bundle_subscription_report' || $page_name == 'bundle_invoice') echo 'active'; ?>">
+				<a href="javascript: void(0);" class="side-nav-link <?php if ($page_name == 'courses' || $page_name == 'course_add' || $page_name == 'course_edit' || $page_name == 'categories' || $page_name == 'category_add' || $page_name == 'category_edit' || $page_name == 'coupons' || $page_name == 'coupon_add' || $page_name == 'coupon_edit') : ?> active <?php endif; ?>">
+					<i class="dripicons-archive"></i>
+					<span> <?php echo get_phrase('courses'); ?> </span>
+					<span class="menu-arrow"></span>
+				</a>
+				<ul class="side-nav-second-level" aria-expanded="false">
+					<?php if (has_permission('course')) : ?>
+						<li class="<?php if ($page_name == 'courses' || $page_name == 'course_edit') echo 'active'; ?>">
+							<a href="<?php echo site_url('admin/courses'); ?>"><?php echo get_phrase('manage_courses'); ?></a>
+						</li>
+					<?php endif; ?>
+
+					<?php if (has_permission('course')) : ?>
+						<li class="<?php if ($page_name == 'course_add') echo 'active'; ?>">
+							<a href="<?php echo site_url('admin/course_form/add_course'); ?>"><?php echo get_phrase('add_new_course'); ?></a>
+						</li>
+					<?php endif; ?>
+
+					<?php if (has_permission('category')) : ?>
+						<li class="<?php if ($page_name == 'categories' || $page_name == 'category_add' || $page_name == 'category_edit') echo 'active'; ?>">
+							<a href="<?php echo site_url('admin/categories'); ?>"><?php echo get_phrase('course_category'); ?></a>
+						</li>
+					<?php endif; ?>
+				</ul>
+			</li>
+		<?php endif; ?>
+
+		<?php if (has_permission('enrolment')) : ?>
+			<li class="side-nav-item <?php if ($page_name == 'enrol_history' || $page_name == 'enrol_student') : ?> active <?php endif; ?>">
+				<a href="javascript: void(0);" class="side-nav-link <?php if ($page_name == 'enrol_history' || $page_name == 'enrol_student') : ?> active <?php endif; ?>">
+					<i class="dripicons-network-3"></i>
+					<span> <?php echo get_phrase('enrolment'); ?> </span>
+					<span class="menu-arrow"></span>
+				</a>
+				<ul class="side-nav-second-level" aria-expanded="false">
+					<li class="<?php if ($page_name == 'enrol_history') echo 'active'; ?>">
+						<a href="<?php echo site_url('admin/enrol_history'); ?>"><?php echo get_phrase('enrol_history'); ?></a>
+					</li>
+				</ul>
+			</li>
+		<?php endif; ?>
+
+		
+
+		<?php if (has_permission('user')) : ?>
+			<li class="side-nav-item <?php if ($page_name == 'admins' || $page_name == 'admin_add' || $page_name == 'admin_edit' || $page_name == 'admin_permission' || $page_name == 'instructors' || $page_name == 'instructor_add' || $page_name == 'instructor_edit' || $page_name == 'instructor_payout' || $page_name == 'instructor_settings' || $page_name == 'application_list' || $page_name == 'users' || $page_name == 'user_add' || $page_name == 'user_edit') : ?> active <?php endif; ?>">
+				<a href="javascript: void(0);" class="side-nav-link <?php if ($page_name == 'admins' || $page_name == 'admin_add' || $page_name == 'admin_edit' || $page_name == 'admin_permission' || $page_name == 'instructors' || $page_name == 'instructor_add' || $page_name == 'instructor_edit' || $page_name == 'instructor_payout' || $page_name == 'instructor_settings' || $page_name == 'application_list' || $page_name == 'users' || $page_name == 'user_add' || $page_name == 'user_edit') : ?> active <?php endif; ?>">
+					<i class="dripicons-box"></i>
+					<span> <?php echo get_phrase('users'); ?> </span>
+					<span class="menu-arrow"></span>
+				</a>
+				<ul class="side-nav-second-level" aria-expanded="false">
+					<?php if (has_permission('admins')) : ?>
+						<li class="side-nav-item <?php if ($page_name == 'admins' || $page_name == 'admin_add' || $page_name == 'admin_edit' || $page_name == 'admin_permission') : ?> active <?php endif; ?>">
+							<a href="javascript: void(0);" class="<?php if ($page_name == 'admins' || $page_name == 'admin_add' || $page_name == 'admin_edit' || $page_name == 'admin_permission') : ?> active <?php endif; ?>" aria-expanded="false"><?php echo get_phrase('admins'); ?>
+								<span class="menu-arrow"></span>
+							</a>
+							<ul class="side-nav-third-level" aria-expanded="false">
+								<li class="<?php if ($page_name == 'admins' || $page_name == 'admin_edit' || $page_name == 'admin_permission') : ?> active <?php endif; ?>">
+									<a href="<?php echo site_url('admin/admins'); ?>" class="<?php if ($page_name == 'admins' || $page_name == 'admin_edit' || $page_name == 'admin_permission') : ?> active <?php endif; ?>"><?php echo get_phrase('manage_admins'); ?></a>
+								</li>
+								<li class="<?php if ($page_name == 'admin_add') echo 'active'; ?>">
+									<a href="<?php echo site_url('admin/admin_form/add_admin_form'); ?>"><?php echo get_phrase('add_new_admin'); ?></a>
+								</li>
+							</ul>
+						</li>
+					<?php endif; ?>
+
+					
+
+					<?php if (has_permission('student')) : ?>
+						<li class="side-nav-item <?php if ($page_name == 'users' || $page_name == 'user_add' || $page_name == 'user_edit') : ?> active <?php endif; ?>">
+							<a href="javascript: void(0);" aria-expanded="false" class="<?php if ($page_name == 'users' || $page_name == 'user_add' || $page_name == 'user_edit') : ?> active <?php endif; ?>"><?php echo get_phrase('students'); ?>
+								<span class="menu-arrow"></span>
+							</a>
+							<ul class="side-nav-third-level" aria-expanded="false">
+								<li class="<?php if ($page_name == 'users' || $page_name == 'user_edit') echo 'active'; ?>">
+									<a href="<?php echo site_url('admin/users'); ?>"><?php echo get_phrase('manage_students'); ?></a>
+								</li>
+								
+							</ul>
+						</li>
+					<?php endif; ?>
+				</ul>
+			</li>
+		<?php endif; ?>
+
+		
+
+		<li class="side-nav-item <?php if ($page_name == 'manage_profile') echo 'active'; ?>">
+			<a href="<?php echo site_url(strtolower($this->session->userdata('role')) . '/manage_profile'); ?>" class="side-nav-link">
+				<i class="dripicons-user"></i>
+				<span><?php echo get_phrase('manage_profile'); ?></span>
+			</a>
+		</li>
+
+		<?php if (has_permission('settings')) : ?>
+			<li class="side-nav-item  <?php if ($page_name == 'system_settings' || $page_name == 'frontend_settings' || $page_name == 'payment_settings' || $page_name == 'smtp_settings' || $page_name == 'manage_language' || $page_name == 'about' || $page_name == 'themes') : ?> active <?php endif; ?>">
+				<a href="javascript: void(0);" class="side-nav-link">
+					<i class="dripicons-toggles"></i>
+					<span> <?php echo get_phrase('settings'); ?> </span>
+					<span class="menu-arrow"></span>
+				</a>
+				<ul class="side-nav-second-level" aria-expanded="false">
+					
+
+					<?php if (addon_status('certificate')) : ?>
+						<li class="<?php if ($page_name == 'certificate_settings') echo 'active'; ?>">
+							<a href="<?php echo site_url('addons/certificate/settings'); ?>"><?php echo get_phrase('certificate_settings'); ?></a>
+						</li>
+					<?php endif; ?>
+
+
+					
+				</ul>
+			</li>
+		<?php endif; ?>
+
+		<?php if (has_permission('messaging')) : ?>
+			<li class="side-nav-item">
+				<a href="<?php echo site_url('admin/message'); ?>" class="side-nav-link <?php if ($page_name == 'message' || $page_name == 'message_new' || $page_name == 'message_read') echo 'active'; ?>">
+					<i class="dripicons-message"></i>
+					<span><?php echo get_phrase('message'); ?></span>
+				</a>
+			</li>
+		<?php endif; ?>
+
+	
+	</ul>
+</div>
---
+++ application/views/frontend/default/includes_bottom.php
@@ -0,0 +1,18 @@
+<script src="<?php echo base_url() . 'assets/frontend/default/js/vendor/modernizr-3.5.0.min.js'; ?>"></script>
+<script src="<?php echo base_url() . 'assets/frontend/default/js/vendor/jquery-3.2.1.min.js'; ?>"></script>
+<script src="<?php echo base_url() . 'assets/frontend/default/js/popper.min.js'; ?>"></script>
+<script src="<?php echo base_url() . 'assets/frontend/default/js/bootstrap.min.js'; ?>"></script>
+
+<?php if ($page_name == "home") : ?>
+	<script src="<?php echo base_url() . 'assets/frontend/default/js/slick.min.js'; ?>"></script>
+	<script src="<?php echo base_url() . 'assets/frontend/default/js/jquery.webui-popover.min.js'; ?>"></script>
+<?php endif; ?>
+
+<?php if ($page_name == "user_profile") : ?>
+	<script src="<?php echo base_url() . 'assets/frontend/default/js/tinymce.min.js'; ?>"></script>
+<?php endif; ?>
+
+<script src="<?php echo base_url() . 'assets/frontend/default/js/main.js'; ?>"></script>
+<script src="<?php echo base_url() . 'assets/global/toastr/toastr.min.js'; ?>"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js" integrity="sha384-FzT3vTVGXqf7wRfy8k4BiyzvbNfeYjK+frTVqZeNDFl8woCbF0CYG6g2fMEFFo/i" crossorigin="anonymous"></script>
+
